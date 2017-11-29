7b77bd166cc2dc53f77b3256d9bdc590c3cf2ebc
github username <github email address>
2017-11-29 15:59:26 -0500

    removed extraneous client folder

--- taskLister_client/index.js
+++ taskLister_client/index.js
@@ -1,21 +1,24 @@
 // const window =   window.onload = getLists()
-let element = document.createElement('p')
-element.innerText = "testing javascript"
-document.body.appendChild(element)
+//window.addEventListener('load', event => console.log(event));
+//
+// let element = document.createElement('p');
+// element.innerText = "testing javascript";
+// document.body.appendChild(element);
 
 function getLists(){
+  //  debugger;
   fetch ('http://localhost:3000/lists')
     .then(res => res.json()).then(json => showList(json))
 }
 
 
 function showList(json){
-  debugger;
-div = document.getElementById('List')
-let ul = document.createElement('ul')
-json.forEach(function(list){
-  link = `<li><a href="${list.url}">${list.name}</a></li>`
-  ul.innertHTML += link
-})
-div.appendChild(ul)
+  // debugger;
+  div = document.getElementById('List')
+  let ul = document.createElement('ul')
+  json.forEach(list => {
+    link = `<li><a href="http://localhost:3000/lists/${list.id}">${list.name}</a></li>`
+    ul.innerHTML += link
+  })
+  div.appendChild(ul)
 }
